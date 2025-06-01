b1326d903835b9ec6c77d318274a93911ae8ab87
SIDDHARTH K SHETTY <shettyksiddharth@gmail.com>
2025-06-01 12:19:52 +0530

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Hero from './Pages/Hero';
+import AfterAnalysis from './Pages/AfterAnalysis';
 
 function App() {
   return (
-    <div className='text-blue-500'>
-      hello
-    </div>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/AfterAnalysis" element={<AfterAnalysis />} />
+    </Routes>
   )
 }
 
---
+++ src/Pages/AfterAnalysis.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AfterAnalysis() {
+  return (
+    <div>
+      after analysis
+    </div>
+  )
+}
+
+export default AfterAnalysis
